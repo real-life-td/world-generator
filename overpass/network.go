502b2bd271f9a4397d17ead0997e96f0468c3659
Joseph Naberhaus <naberhausj@gmail.com>
2020-04-16 11:32:54 -0500

    feat(network.go): add network call to the overpass api

---
+++ overpass/network.go
@@ -0,0 +1,67 @@
+package overpass
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const overpassEndpoint = "https://overpass-api.de/api/interpreter"
+const query = "[bbox:%f,%f,%f,%f];(way[highway];way[building];);out geom;"
+
+func call(query string) (body io.ReadCloser, err error) {
+	resp, err := http.Get(overpassEndpoint + "?data=" + query)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Body, nil;
+}
+
+func makeQuery(lat1, lon1, lat2, lon2 float64) string {
+	if lat1 > lat2 {
+		lat1, lat2 = lat2, lat1
+	}
+
+	if lon1 > lon2 {
+		lon1, lon2 = lon2, lon1
+	}
+
+	return fmt.Sprintf(query, lat1, lon1, lat2, lon2)
+}
+
+func checkCoordinates(lat, lon float64) error {
+	if -90.0 > lat || lat > 90.0 {
+		return errors.New("latitude must be in range -90 to 90 inclusive")
+	}
+
+	if -180.0 > lon || lon > 180.0 {
+		return errors.New("longitude must be in range -180 to 180 inclusive")
+	}
+
+	return nil
+}
+
+func executeQuery(lat1, lon1, lat2, lon2 float64) (result *overpassResult, err error) {
+	err = checkCoordinates(lat1, lon1)
+	if err != nil {
+		return nil, err
+	}
+
+	err = checkCoordinates(lat2, lon2)
+	if err != nil {
+		return nil, err
+	}
+
+	query := makeQuery(lat1, lon1, lat2, lon2)
+	body, err := call(query)
+	if err != nil {
+		return nil, err
+	}
+
+	result = new(overpassResult)
+	json.NewDecoder(body).Decode(result)
+	return result, nil
+}

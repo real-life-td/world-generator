2c90f3ab404cb4d04c6a30e1ad71ae999b5245a7
Joseph Naberhaus <naberhausj@gmail.com>
2020-04-23 20:15:47 -0500

    feat(convert.go): fix convert tests
    Constants renaming and a type change have broken these tests

--- convert/convert_test.go
+++ convert/convert_test.go
@@ -10,7 +10,7 @@ func TestClassify(t *testing.T) {
 	test := func (tags overpass.Tags, expected elementType, shouldError bool) {
 		w := overpass.Way{
 			Id:       0,
-			Bounds:   [4]int{0, 0, 0, 0},
+			Bounds:   [4]float64{0, 0, 0, 0},
 			Nodes:    nil,
 			Geometry: nil,
 			Tags:     &tags,
@@ -26,7 +26,7 @@ func TestClassify(t *testing.T) {
 	}
 
 	test(overpass.Tags{Building: "", Highway: ""}, -1, true)
-	test(overpass.Tags{Building: "yes", Highway: ""}, BUILDING, false)
-	test(overpass.Tags{Building: "", Highway: "primary"}, HIGHWAY, false)
+	test(overpass.Tags{Building: "yes", Highway: ""}, BuildingType, false)
+	test(overpass.Tags{Building: "", Highway: "primary"}, HighwayType, false)
 	test(overpass.Tags{Building: "yes", Highway: "primary"}, -1, true)
 }

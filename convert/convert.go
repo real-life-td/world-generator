be3663f326e986702cdfe2eacdbe4f92daa42920
Joseph Naberhaus <naberhausj@gmail.com>
2020-04-23 20:46:35 -0500

    feat(convert.go): add conversion from overpass result to world

--- convert/convert.go
+++ convert/convert.go
@@ -8,12 +8,46 @@ import (
 
 type elementType int
 const (
-	BUILDING elementType = iota
-	HIGHWAY
+	BuildingType elementType = iota
+	HighwayType
 )
 
-func Convert(result *overpass.Result) (world *world.Container, err error) {
-	return nil, nil
+func Convert(meta *world.Metadata, result *overpass.Result) (w *world.Container, err error) {
+	buildingElements, highwayElements := separate(result.Elements)
+
+	buildings, err := convertBuildings(meta, buildingElements)
+	if err != nil {
+		return nil, err
+	}
+
+	roads, err := convertRoads(meta, highwayElements)
+	if err != nil {
+		return nil, err
+	}
+
+	return world.NewContainer(meta, roads, buildings), nil
+}
+
+func separate(elements []*overpass.Way) (buildings, highways []*overpass.Way) {
+	buildingElements := make([]*overpass.Way, 0, 10)
+	highwayElements := make([]*overpass.Way, 0, len(elements)) // Most elements will end up being roads
+
+	for _, e := range elements {
+		t, err := classify(e)
+		if err != nil {
+			// Overpass data might not be perfect so just ignore this error
+			continue
+		}
+
+		switch t {
+		case BuildingType:
+			buildingElements = append(buildingElements, e)
+		case HighwayType:
+			highwayElements = append(highwayElements, e)
+		}
+	}
+
+	return buildingElements, highwayElements
 }
 
 func classify(e *overpass.Way) (t elementType, err error) {
@@ -22,9 +56,9 @@ func classify(e *overpass.Way) (t elementType, err error) {
 			return -1, errors.New("element is both building and highway type")
 		}
 
-		return BUILDING, nil
+		return BuildingType, nil
 	} else if e.Tags.Highway != "" {
-		return HIGHWAY, nil
+		return HighwayType, nil
 	}
 
 	return -1, errors.New("element does not have type")
--- convert/convert_test.go
+++ convert/convert_test.go
@@ -1,10 +1,72 @@
 package convert
 
 import (
+	"github.com/real-life-td/game-core/world"
 	"github.com/real-life-td/world-generator/overpass"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
+
+func TestConvert(t *testing.T) {
+	metadata := world.NewMetadata(100, 100, 0.0, 0.0, 1.0, 1.0)
+
+	result := overpass.Result{
+		Version:  "1",
+		Elements: []*overpass.Way{
+			{
+				Id:       0,
+				Bounds:   [4]float64{0.0, 0.0, 0.5, 0.5},
+				Nodes:    []uint64{0, 1},
+				Geometry: []*overpass.LatLon{
+					{0.0, 0.0},
+					{0.5, 0.5},
+				},
+				Tags:     &overpass.Tags{Highway: "primary"},
+			},
+			{
+				Id:       1,
+				Bounds:   [4]float64{0.6, 0.6, 1.0, 1.0},
+				Nodes:    []uint64{2, 3, 4, 5},
+				Geometry: []*overpass.LatLon{
+					{0.6, 0.6},
+					{1.0, 0.6},
+					{1.0, 1.0},
+					{0.6, 1.0},
+				},
+				Tags:     &overpass.Tags{Building: "yes"},
+			},
+		},
+	}
+
+	makeId := func(baseId uint64, idType world.Type) world.Id {
+		id, err := world.NewId(baseId, idType)
+		require.NoError(t, err)
+		return id
+	}
+
+	expectedRoads := []*world.Road{
+		world.NewRoad(
+			makeId(0, world.RoadType),
+			world.NewNode(makeId(0, world.NodeType), 0, 0),
+			world.NewNode(makeId(1, world.NodeType), 50, 50),
+			1),
+	}
+
+	expectedBuildings := []*world.Building{
+		world.NewBuilding(makeId(1, world.BuildingType), []*world.Node{
+			world.NewNode(makeId(2, world.NodeType), 60, 60),
+			world.NewNode(makeId(3, world.NodeType), 60, 100),
+			world.NewNode(makeId(4, world.NodeType), 100, 100),
+			world.NewNode(makeId(5, world.NodeType), 100, 60),
+		}),
+	}
+
+	expectedContainer := world.NewContainer(metadata, expectedRoads, expectedBuildings)
+
+	container, err := Convert(metadata, &result)
+	require.NoError(t, err)
+	require.Equal(t, expectedContainer, container)
+}
 
 func TestClassify(t *testing.T) {
 	test := func (tags overpass.Tags, expected elementType, shouldError bool) {
